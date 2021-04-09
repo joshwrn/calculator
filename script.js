55700866ec3529d190e3e18fb5b8616dc1ab7057
joshwrn <joshnwarren@gmail.com>
2021-04-09 13:37:58 -0700

    UI mostly complete

---
+++ script.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(opperation, num1, num2) {
+    
+}
+
+console.log(add(1, 7));
+console.log(divide(1, 7));
